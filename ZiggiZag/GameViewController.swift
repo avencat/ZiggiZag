00b760bc56e4e1899e8686dabe438ba9043cf5ab
Axel Vencatareddy <avencatareddy@me.com>
2016-10-30 14:13:04 +0100

    Game almost finished

--- ZiggiZag/GameViewController.swift
+++ ZiggiZag/GameViewController.swift
@@ -10,116 +10,168 @@ import UIKit
 import QuartzCore
 import SceneKit
 
-class GameViewController: UIViewController {
+class GameViewController: UIViewController, SCNSceneRendererDelegate {
+  
+  let scene = SCNScene()
+  let cameraNode = SCNNode()
+  var person = SCNNode()
+  let firstBox = SCNNode()
+  var goingLeft = Bool()
+  var tempBox = SCNNode()
+  var prevBoxNumber = Int()
+  var boxNumber = Int()
+  
+  override func viewDidLoad() {
+    self.createScene()
+    
+  }
+  func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // create and add a camera to the scene
-        let cameraNode = SCNNode()
-        cameraNode.camera = SCNCamera()
-        scene.rootNode.addChildNode(cameraNode)
-        
-        // place the camera
-        cameraNode.position = SCNVector3(x: 0, y: 0, z: 15)
-        
-        // create and add a light to the scene
-        let lightNode = SCNNode()
-        lightNode.light = SCNLight()
-        lightNode.light!.type = .omni
-        lightNode.position = SCNVector3(x: 0, y: 10, z: 10)
-        scene.rootNode.addChildNode(lightNode)
-        
-        // create and add an ambient light to the scene
-        let ambientLightNode = SCNNode()
-        ambientLightNode.light = SCNLight()
-        ambientLightNode.light!.type = .ambient
-        ambientLightNode.light!.color = UIColor.darkGray
-        scene.rootNode.addChildNode(ambientLightNode)
-        
-        // retrieve the ship node
-        let ship = scene.rootNode.childNode(withName: "ship", recursively: true)!
-        
-        // animate the 3d object
-        ship.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 2, z: 0, duration: 1)))
-        
-        // retrieve the SCNView
-        let scnView = self.view as! SCNView
-        
-        // set the scene to the view
-        scnView.scene = scene
-        
-        // allows the user to manipulate the camera
-        scnView.allowsCameraControl = true
-        
-        // show statistics such as fps and timing information
-        scnView.showsStatistics = true
-        
-        // configure the view
-        scnView.backgroundColor = UIColor.black
-        
-        // add a tap gesture recognizer
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
-        scnView.addGestureRecognizer(tapGesture)
+    let deleteBox = self.scene.rootNode.childNode(withName: "\(prevBoxNumber)", recursively: true)
+    
+    let currentBox = self.scene.rootNode.childNode(withName: "\(prevBoxNumber + 1)", recursively: true)
+    
+    if (deleteBox?.position.x)! > person.position.x + 1 || (deleteBox?.position.z)! > person.position.z + 1{
+      prevBoxNumber+=1
+      deleteBox?.removeFromParentNode()
+      
+      createBox()
     }
     
-    func handleTap(_ gestureRecognize: UIGestureRecognizer) {
-        // retrieve the SCNView
-        let scnView = self.view as! SCNView
+    if person.position.x > (currentBox?.position.x)! - 0.5 && person.position.x < (currentBox?.position.x)! + 0.5 || person.position.z > (currentBox?.position.z)! - 0.5 && person.position.z < (currentBox?.position.z)! + 0.5{
+      
+      //On Platform
+      
+    }
+    else{
+      die()
+      
+      
+    }
+    
+    
+    
+  }
+  
+  func die(){
+    person.runAction(SCNAction.move(to: SCNVector3Make(person.position.x, person.position.y - 10, person.position.z), duration: 0.5))
+    let wait = SCNAction.wait(duration: 1.0)
+    let sequence = SCNAction.sequence([wait, SCNAction.run({
+      node in
+      self.scene.rootNode.enumerateChildNodes({
+        node, stop in
         
-        // check what nodes are tapped
-        let p = gestureRecognize.location(in: scnView)
-        let hitResults = scnView.hitTest(p, options: [:])
-        // check that we clicked on at least one object
-        if hitResults.count > 0 {
-            // retrieved the first clicked object
-            let result: AnyObject = hitResults[0]
-            
-            // get its material
-            let material = result.node!.geometry!.firstMaterial!
-            
-            // highlight it
-            SCNTransaction.begin()
-            SCNTransaction.animationDuration = 0.5
-            
-            // on completion - unhighlight
-            SCNTransaction.completionBlock = {
-                SCNTransaction.begin()
-                SCNTransaction.animationDuration = 0.5
-                
-                material.emission.contents = UIColor.black
-                
-                SCNTransaction.commit()
-            }
-            
-            material.emission.contents = UIColor.red
-            
-            SCNTransaction.commit()
-        }
-    }
-    
-    override var shouldAutorotate: Bool {
-        return true
-    }
-    
-    override var prefersStatusBarHidden: Bool {
-        return true
-    }
-    
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
+        
+        node.removeFromParentNode()
+      })
+    }), SCNAction.run({
+      node in
+      
+      self.createScene()
+    })])
+    person.runAction(sequence)
+  }
 
+  func createBox(){
+    tempBox = SCNNode(geometry: firstBox.geometry)
+    let prevBox = scene.rootNode.childNode(withName: "\(boxNumber)", recursively: true)
+    boxNumber += 1
+    tempBox.name = "\(boxNumber)"
+    let randomNumber = arc4random() % 2
+    
+    switch randomNumber{
+    case 0:
+      tempBox.position = SCNVector3Make((prevBox?.position.x)! - firstBox.scale.x, (prevBox?.position.y)!, (prevBox?.position.z)!)
+      break
+    case 1:
+      tempBox.position = SCNVector3Make((prevBox?.position.x)!, (prevBox?.position.y)!, (prevBox?.position.z)! - firstBox.scale.z)
+      break
+      
+    default:
+      
+      break
+    }
+    
+    self.scene.rootNode.addChildNode(tempBox)
+    
+    
+    
+  }
+  
+  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    if goingLeft == false {
+      person.removeAllActions()
+      person.runAction(SCNAction.repeatForever(SCNAction.move(by: SCNVector3Make(-50, 0, 0), duration: 20)))
+      goingLeft = true
+    }
+      
+    else {
+      person.removeAllActions()
+      person.runAction(SCNAction.repeatForever(SCNAction.move(by: SCNVector3Make(0, 0, -50), duration: 20)))
+      goingLeft = false
+      
+    }
+  }
+  
+  func createScene(){
+    boxNumber = 0
+    prevBoxNumber = 0
+    
+    
+    self.view.backgroundColor = UIColor.white
+    let sceneView = self.view as! SCNView
+    sceneView.delegate = self
+    sceneView.scene = scene
+    
+    //Create Person
+    
+    let personGeo = SCNSphere(radius: 0.2)
+    person = SCNNode(geometry: personGeo)
+    let personMat = SCNMaterial()
+    personMat.diffuse.contents = UIColor.red
+    personGeo.materials = [personMat]
+    person.position = SCNVector3Make(0, 1.1, 0)
+    scene.rootNode.addChildNode(person)
+    
+    //Create Camera
+    cameraNode.camera = SCNCamera()
+    cameraNode.camera?.usesOrthographicProjection = true
+    cameraNode.camera?.orthographicScale = 3
+    cameraNode.position = SCNVector3Make(20, 20, 20)
+    cameraNode.eulerAngles = SCNVector3Make(-45, 45, 0)
+    let constraint = SCNLookAtConstraint(target: person)
+    constraint.isGimbalLockEnabled = true
+    self.cameraNode.constraints = [constraint]
+    scene.rootNode.addChildNode(cameraNode)
+    person.addChildNode(cameraNode)
+    
+    
+    //Create Box
+    let firstBoxGeo = SCNBox(width: 1, height: 1.5, length: 1, chamferRadius: 0)
+    firstBox.geometry = firstBoxGeo
+    let boxMaterial = SCNMaterial()
+    boxMaterial.diffuse.contents = UIColor(red: 0.2, green: 0.8, blue: 0.9, alpha: 1.0)
+    firstBoxGeo.materials = [boxMaterial]
+    firstBox.position = SCNVector3Make(0, 0, 0)
+    scene.rootNode.addChildNode(firstBox)
+    firstBox.name = "\(boxNumber)"
+    
+    for _ in 0...8{
+      createBox()
+    }
+    
+    //Create Light
+    let light = SCNNode()
+    light.light = SCNLight()
+    light.light?.type = SCNLight.LightType.directional
+    light.eulerAngles = SCNVector3Make(-45, 45, 0)
+    scene.rootNode.addChildNode(light)
+    
+    let light2 = SCNNode()
+    light2.light = SCNLight()
+    light2.light?.type = SCNLight.LightType.directional
+    light2.eulerAngles = SCNVector3Make(45, 45, 0)
+    scene.rootNode.addChildNode(light2)
+  }
+  
 }
